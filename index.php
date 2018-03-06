fbfb307a9899a91d73143420ff2186120998a051
Rafael Damásio <rafaelcezario@gmail.com>
2018-03-06 12:06:33 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php
   $login_cookie = $_COOKIE['login'];
-    if(isset($login_cookie)){
-      echo"Bem vindo, $login_cookie <br> !!!\n";
+    if(isset($login_cookie))
+    {
+      echo"Bem vindo, $login_cookie <br> !!!";
       echo"\n Essas informações <font color='red'>PODEM</font> ser acessadas por você";
-    }else{
-      echo"Bem-Vindo, convidado <br>";
-      echo"Essas informações <font color='red'>NÃO PODEM</font> ser acessadas por você";
-      echo"Para ler o conteúdo é necessário efetuar o login \n";
-      echo"(<br><a href='login.html'>Login</a>)";
+    }
+    else
+    {
+      echo"Bem vindo, convidado <br>";
+      echo"\n Essas informações <font color='red'>NÃO PODEM</font> ser acessadas por você";
+      echo"\n Para ler o conteúdo é necessário efetuar o login \n";
+      echo"<br>**** <a href='login.html'> Clique aqui</a> ****";
     }
 ?>
