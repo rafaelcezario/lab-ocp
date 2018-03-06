7e0613c13993b316ce7e78cfca4c32886af2a7d9
Rafael Damásio <rafaelcezario@gmail.com>
2018-03-06 12:09:32 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
     if(isset($login_cookie))
     {
       echo"Bem vindo, $login_cookie <br> !!!";
-      echo"\n Essas informações <font color='red'>PODEM</font> ser acessadas por você";
+      echo"\n Essas informações <font color='red'>PODEM</font> ser acessadas por você.";
     }
     else
     {
       echo"Bem vindo, convidado <br>";
-      echo"\n Essas informações <font color='red'>NÃO PODEM</font> ser acessadas por você";
-      echo"\n Para ler o conteúdo é necessário efetuar o login \n";
+      echo"\nEssas informações <font color='red'>NÃO PODEM</font> ser acessadas por você.\n";
+      echo"\nPara ler o conteúdo é necessário efetuar o login. \n";
       echo"<br>**** <a href='login.html'> Clique aqui</a> ****";
     }
 ?>
