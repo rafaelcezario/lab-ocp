0baef67b7432ce38bcb17d6a864d1a7c6a7244dd
Rafael Damásio <rafaelcezario@gmail.com>
2018-03-07 08:36:01 -0300

    Alteração do index - Formatação

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
   $login_cookie = $_COOKIE['login'];
+  
+  echo "<h2> Demonstração Openshift na prática!!! <br><br>"
+  echo $_SERVER['SERVER_ADDR'];
+
     if(isset($login_cookie))
     {
       echo"Bem vindo, $login_cookie <br> !!!";
@@ -8,8 +12,9 @@
     else
     {
       echo"Bem vindo, convidado <br>";
-      echo"<br> Essas informações <font color='red'>NÃO PODEM</font> ser acessadas por você.";
-      echo"<br> Para ler o conteúdo é necessário efetuar o login. \n";
-      echo"\r\n<br>**** <a href='login.html'> Clique aqui</a> ****";
+      echo" Essas informações <font color='red'>NÃO PODEM</font> ser acessadas por você.<br>";
+      echo" Para ler o conteúdo é necessário efetuar o login. <br>";
+      echo"<br><h2> <a href='login.html'> Clique aqui</a>";
     }
+
 ?>
