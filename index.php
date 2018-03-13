f3d64d20a1537e2301ec9b6d73e0a8d829c3ec2b
Rafael Damásio <rafaelcezario@gmail.com>
2018-03-12 22:55:28 -0300

    Incluido a imagem do disco.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
   $login_cookie = $_COOKIE['login'];
-  $image='https://blog.openshift.com/wp-content/uploads/Logotype_RH_OpenShiftContainerPlatform_wLogo_CMYK_Black.jpg';
-
+  #$image='https://blog.openshift.com/wp-content/uploads/Logotype_RH_OpenShiftContainerPlatform_wLogo_CMYK_Black.jpg';
+  $image="images/logo_openshift.jpg";
 	echo "<h2> Demonstração Openshift na prática!!! <br><br></h2>";
 	echo '<img src="'.$image.'" width="400" height="80" /><br>';
 	echo $_SERVER['SERVER_ADDR'];
